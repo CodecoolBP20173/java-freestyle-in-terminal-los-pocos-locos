73741a4df947ca27b2e5b209e146785745ac3867
Horinka Peter <horinka.peter@gmail.com>
2018-03-12 14:39:22 +0100

    started implementing logic

---
+++ TicTacToe.java
@@ -0,0 +1,22 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class MyClass{
+    public static Boolean WinCheck(String a) {
+        for (i = 0; i< GameBoard.length; i++);
+
+            for (j = 0; j < GameBoard[i].length; j++);
+                if (GameBoard[i][j] == a) {
+                    return true;
+                }
+    }     
+
+    public static void main(String[] args) {
+        private static String[][] GameBoard = new String[][]{
+            {"_","_","_"},
+            {"_","_","_"},
+            {"_","_","_"}
+        };
+
+                   
+    }
+}
