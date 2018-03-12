1ce1466d1f4ace10d9629fc4676558c29be18eeb
Horinka Peter <horinka.peter@gmail.com>
2018-03-12 15:08:22 +0100

    diag/horiz winchecks implemented

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,32 @@
 import com.sun.org.apache.xpath.internal.operations.String;
 
 public class MyClass{
+    public static void main(String[] args) {
+        String[][] GameBoard = new String[][]{
+            {"_","_","_"},
+            {"_","_","_"},
+            {"_","_","_"}
+        };
+    }         
+    
     public static Boolean WinCheck(String a, String[][] board) {
         
     }
     
     public static Boolean horizontalCheck(String a, String[][] board) {
-
+        for (i = 0; i < board.length; i++) {
+            if (board[i][0] == a && board[i][1] == a && board[i][2] == a) {
+                return true;
+            }
+        }
     }
 
     public static Boolean verticalCheck(String a, String[][] board) {
-
+        for (i = 0; i < board.length; i++) {
+            if (board[0][i] == a && board[1][i] == a && board[2][i] == a) {
+                return true;
+            }
+        }
     }
 
     public static Boolean diagonalDownwardslCheck(String a, String[][] board) {
@@ -21,11 +37,4 @@ public class MyClass{
 
     }
 
-    public static void main(String[] args) {
-        String[][] GameBoard = new String[][]{
-            {"_","_","_"},
-            {"_","_","_"},
-            {"_","_","_"}
-        };         
-    }
 }
