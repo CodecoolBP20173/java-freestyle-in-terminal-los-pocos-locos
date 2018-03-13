5751ecbc8ba200b7662cde8db172dca344131751
Erik Szigeti <erik.szigeti01@gmail.com>
2018-03-13 10:12:08 +0100

    diagonal checking and minor changes to horizontal and vertical checking

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,7 +10,7 @@ import TerminalManipulation.src.com.codecool.termlib.Direction;
 public class TicTacToe {
 
     public static void main(String[] args) {
-        String[][] board = new String[][] { { "_", "x", "_" }, { "_", "_", "_" }, { "_", "_", "_" } };
+        String[][] board = new String[][] { { "-", "-", "-" , "-"}, { "-", "-", "-" , "-"},{ "-", "-", "-" , "-"},{ "-", "-", "-" ,"-"} };
         Terminal term = new Terminal();
         term.printField(board);
     }
@@ -20,39 +20,36 @@ public class TicTacToe {
     }
 
     public static Boolean horizontalCheck(String a, String[][] board) {
-        for (i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][0] == a && board[i][1] == a && board[i][2] == a) {
                 return true;
             }
         }
+        return false;
     }
 
     public static Boolean verticalCheck(String a, String[][] board) {
-        for (i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[0][i] == a && board[1][i] == a && board[2][i] == a) {
                 return true;
             }
         }
-
+        return false;
     }
 
     public static Boolean diagonalDownwardslCheck(String a, String[][] board) {
         String lastItem = a;
-        int count = 0;
+        int matchCount = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (i == j) {
-                    if (i == 0 && j == 0) {
-                        lastItem = board[i][j];
-                    } else {
-                        if (board[i][j] == lastItem) {
-                            count++;
-                        }
+                    if (lastItem == board[i][j]) {
+                        matchCount++;
                     }
                 }
             }
         }
-        if (count == board.length - 1) {
+        if (matchCount == board.length) {
             return true;
         } else {
             return false;
