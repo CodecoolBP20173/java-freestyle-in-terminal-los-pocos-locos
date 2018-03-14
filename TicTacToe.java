41ec85bf52696b682195837b11d42a053dd33a9b
Horváth Máté <ifjmate@gmail.com>
2018-03-14 11:09:08 +0100

    Add input error handling

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,47 +8,46 @@ import TerminalManipulation.src.com.codecool.termlib.Direction;
 
 import java.util.Arrays;
 import java.util.Scanner;
-//import com.sun.org.apache.regexp.internal.recompile;
-//import com.sun.org.apache.xpath.internal.operations.String;
 
 public class TicTacToe {
+
+    public static String[][] board = generateBoard(3);
 
     public static Terminal term = new Terminal();
 
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
-        String[][] board = generateBoard(3);
         term.clearScreen();
-        term.printField(board);  
+        term.printField(board);
         while (true) {
-            String mynumb = userInput(user_input, "x");
+            int mynumb = userInput(user_input, "x");
             int[] kecske = decisionIndexChanger(mynumb, board.length);
-            move("x", kecske, board);
+            move("x", kecske);
             term.clearScreen();
             term.printField(board);
-            if (winCheck("x", board)){
+            if (winCheck("x")) {
                 break;
             }
             mynumb = userInput(user_input, "o");
             kecske = decisionIndexChanger(mynumb, board.length);
-            move("o", kecske, board);
+            move("o", kecske);
             term.clearScreen();
             term.printField(board);
-            if (winCheck("o", board)){
+            if (winCheck("o")) {
                 break;
             }
-        } 
-        user_input.close();    
+        }
+        user_input.close();
     }
 
-    public static Boolean winCheck(String a, String[][] board) {
-        if (horizontalCheck(a, board) || verticalCheck(a, board) || diagonalDownwardslCheck(a, board) || diagonalUpwardsCheck(a, board)){
+    public static Boolean winCheck(String a) {
+        if (horizontalCheck(a) || verticalCheck(a) || diagonalDownwardslCheck(a) || diagonalUpwardsCheck(a)) {
             return true;
         }
         return false;
     }
 
-    public static Boolean horizontalCheck(String a, String[][] board) {
+    public static Boolean horizontalCheck(String a) {
         for (int i = 0; i < board.length; i++) {
             if (board[i][0] == a && board[i][1] == a && board[i][2] == a) {
                 return true;
@@ -57,7 +56,7 @@ public class TicTacToe {
         return false;
     }
 
-    public static Boolean verticalCheck(String a, String[][] board) {
+    public static Boolean verticalCheck(String a) {
         for (int i = 0; i < board.length; i++) {
             if (board[0][i] == a && board[1][i] == a && board[2][i] == a) {
                 return true;
@@ -66,7 +65,7 @@ public class TicTacToe {
         return false;
     }
 
-    public static Boolean diagonalDownwardslCheck(String lastItem, String[][] board) {
+    public static Boolean diagonalDownwardslCheck(String lastItem) {
         int matchCount = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
@@ -84,7 +83,7 @@ public class TicTacToe {
         }
     }
 
-    public static Boolean diagonalUpwardsCheck(String lastItem, String[][] board) {
+    public static Boolean diagonalUpwardsCheck(String lastItem) {
         int matchCount = 0;
         for (int i = board.length - 1; i >= 0; i--) {
             for (int j = 0; j < board.length; j++) {
@@ -114,26 +113,28 @@ public class TicTacToe {
         return board;
     }
 
-    public static int[] decisionIndexChanger(String decision, int boardsize) {
-        int decisionInt = Integer.parseInt(decision);
-        int rowIndex = decisionInt / boardsize;
-        int tempColumnIndex = decisionInt % boardsize;
+    public static int[] decisionIndexChanger(Integer decision, int boardsize) {
+        int rowIndex = decision / boardsize;
+        int tempColumnIndex = decision % boardsize;
         int columnIndex;
         if (tempColumnIndex == 0) {
             columnIndex = boardsize - 1;
             rowIndex--;
         } else {
-            columnIndex = (decisionInt % boardsize) - 1;
+            columnIndex = (decision % boardsize) - 1;
         }
         int[] array = new int[] { rowIndex, columnIndex };
         return array;
     }
 
-    public static String userInput(Scanner user_input ,String player) {
+    public static Integer userInput(Scanner user_input, String player) {
         while (true) {
             try {
                 System.out.println(player + "'s turn:");
-                String number = user_input.next();
+                Integer number = user_input.nextInt();
+                if (!validMove(player, number)) {
+                  continue;
+                }
                 return number;
             } catch (java.util.InputMismatchException e) {
                 user_input.nextLine();
@@ -143,7 +144,16 @@ public class TicTacToe {
 
     }
 
-    public static void move(String player, int[] coords, String[][] board) {
+    public static void move(String player, int[] coords) {
         board[coords[0]][coords[1]] = player;
     }
+
+    public static Boolean validMove(String player, Integer input){
+        int[] coordinate = decisionIndexChanger(input, 3);
+        if(board[coordinate[0]][coordinate[1]] == "x" || board[coordinate[0]][coordinate[1]] == "o"){
+            return false;
+        }
+        return true;
+    }
+   
 }
